33fafbb9aafb9049770f66b531e36e6820acc7db
CRESPY Arthur <arthur@crespy.eu>
2023-03-23 12:36:49 +0100

    Improve eat and think functions

--- files/srcs/philo.c
+++ files/srcs/philo.c
@@ -14,13 +14,22 @@
 
 void    think(t_data *data)
 {
-	printf("%d %d is thinking\n", ft_timecode(data), data->philo->id);
+	if (data->philo->eat_last + data->time_to_sleep + data->time_to_eat < ft_timecode(data) - 10)
+	{
+		printf("%d %d is thinking\n", ft_timecode(data), data->philo->id);
+		while (data->philo->eat_last + data->time_to_sleep + data->time_to_eat < ft_timecode(data) - 10)
+			;
+	}
+	else
+		eat(data);
+	sleeep(data);
 }
 
 void    sleeep(t_data *data)
 {
 	printf("%d %d is sleeping\n", ft_timecode(data), data->philo->id);
 	usleep(data->time_to_sleep * 1000);
+	think(data);
 }
 
 void eat(t_data *data)
@@ -28,29 +37,52 @@ void eat(t_data *data)
 	int id;
 
 	id = data->philo->id;
-	pthread_mutex_lock(&data->philo->fork);
-	printf("%d %d has taken a fork\n", ft_timecode(data), data->philo->id);
+	pthread_mutex_lock(&data->data_access);
+	if (data->philo->fork == 0)
+	{
+		go_to_philo(&data, data->philo->id - 1);
+		if (data->philo->fork == 0)
+		{
+			data->philo->fork = 1;
+			printf("%d %d has taken a fork\n", ft_timecode(data), data->philo->id + 1);
+			go_to_philo(&data, id);
+			data->philo->fork = 1;
+			printf("%d %d has taken a fork\n", ft_timecode(data), data->philo->id);
+			data->philo->eat_last = ft_timecode(data);
+			data->philo->eat_count++;
+			printf("%d %d is eating\n", data->philo->eat_last, data->philo->id);
+			pthread_mutex_unlock(&data->data_access);
+			usleep(data->time_to_eat * 1000);
+		}
+		else
+		{
+			go_to_philo(&data, id);
+			pthread_mutex_unlock(&data->data_access);
+			think(data);
+		}
+	}
+	else
+	{
+		go_to_philo(&data, id);
+		pthread_mutex_unlock(&data->data_access);
+		think(data);
+	}
+	pthread_mutex_lock(&data->data_access);
+	data->philo->fork = 0;
 	go_to_philo(&data, data->philo->id - 1);
-	pthread_mutex_lock(&data->philo->fork);
-	printf("%d %d has taken a fork\n", ft_timecode(data), data->philo->id);
+	data->philo->fork = 0;
 	go_to_philo(&data, id);
-	data->philo->last_eat = ft_timecode(data);
-	printf("%d %d is eating\n", data->philo->last_eat, data->philo->id);
-	usleep(data->time_to_eat * 1000);
-	pthread_mutex_unlock(&data->philo->fork);
-	go_to_philo(&data, data->philo->id - 1);
-	pthread_mutex_unlock(&data->philo->fork);
+	pthread_mutex_unlock(&data->data_access);
+	sleeep(data);
 }
 
 void	*philosopher(void *arg)
 {
-	int		i;
 	t_data	*data;
 
-	i = 0;
 	data = (t_data*)arg;
+	while (data->all_alive == 0)
+		data = (t_data*)arg;
 	eat(data);
-	think(data);
-	sleeep(data);
 	return (NULL);
 }
